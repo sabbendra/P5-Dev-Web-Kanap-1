c37e0e6281566aeed453d75bed3d56bb5cdc72c3
Sabrina Kessar <sabbendra@gmail.com>
2022-08-21 09:10:40 +0200

    fonction clear pour le localStorage

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,16 +1,20 @@
- const orderId = getOrderId()
- displayOrderId(orderId)
+const orderId = getOrderId()
+displayOrderId(orderId)
+clearAllCache()
 
-     //Récupérer le numéro de commande
- function getOrderId() {
+    //Récupérer le numéro de commande
+function getOrderId() {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const orderId = urlSearchParams.get("id");
     return orderId
- }
-     //Afficher le numéro de commande
- function displayOrderId(orderId) {
+}
+    //Afficher le numéro de commande
+function displayOrderId(orderId) {
     const orderIdElement = document.getElementById("orderId")
     orderIdElement.textContent = orderId
- }
- 
-localStorage.clear();
+}
+    //localStorage clear
+function clearAllCache() {
+    const cache = window.localStorage
+    cache.clear()
+}
